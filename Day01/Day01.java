9079647063a245aa2f2d4bf899a6e6a063b3754b
colleen-osgood <coco83579@icloud.com>
2022-12-02 13:53:24 -0600

    pt2 done

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day01 {
 
@@ -39,11 +41,44 @@ public class Day01 {
     
 
     public static void part2() {
+
+        Scanner in;
+        try{
+            in = new Scanner(new File(DATA_FILE));
+        } catch(Exception e) {
+            System.out.println(e);
+            return;
+        }
+
+        ArrayList<Integer> calorieList = new ArrayList<Integer>();
+
+        while(in.hasNext()){
+            String line = in.nextLine();
+            int calories = 0;
+            
+            while(!line.equals("")){
+                calories += Integer.parseInt(line);
+                if(in.hasNext()){ 
+                    line = in.nextLine();  
+                } else {
+                    break;
+                }
+            }
+
+            calorieList.add(calories);
+
+
+        }
+
+        Collections.sort(calorieList);
+        Collections.reverse(calorieList);
+        System.out.println(calorieList.remove(0) + calorieList.remove(0) + calorieList.remove(0));
+        
         
     }
 
     public static void main(String[] args) {
-        part1();
+        part2();
     }
 
 
