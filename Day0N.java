b06d67fe98d5374ac1ad0db59928c6fa0a920a21
colleen-osgood <coco83579@icloud.com>
2022-12-05 14:26:39 -0600

    pt1 done

---
+++ Day0N.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+
+public class Day03{
+
+
+
+
+    public static void part1() throws IOException{
+   
+
+    }
+
+    public static void part2() throws IOException {
+       
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            part2();
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+}
