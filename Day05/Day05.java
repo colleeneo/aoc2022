f70c60ea011a81a3a975f33bb8faef1c96651c60
colleen-osgood <coco83579@icloud.com>
2022-12-07 14:23:06 -0600

    pt2 done

--- Day05/Day05.java
+++ Day05/Day05.java
@@ -93,13 +93,72 @@ public class Day05{
     }
 
     public static void part2() throws IOException {
-       
+        ArrayList<Stack<String>> stacks = new ArrayList<Stack<String>>();
+   
+        Scanner in = new Scanner(new File("input.txt"));
+
+        String line = in.nextLine();
+        int numStacks = line.length() /4 + 1;
+
+        for(int i = 0; i < numStacks; i++){
+            stacks.add(new Stack<String>());
+        }
+        putStuffInStacks(line, stacks);
+        System.out.println(stacks);
+        boolean done = false;
+
+        while(in.hasNext() && !done){
+            line = in.nextLine();
+            if(!line.substring(1,2).equals("1")){
+                putStuffInStacks(line, stacks);
+            }else{
+                done = true;
+            }
+        }
+
+        for(Stack stack : stacks) {
+            Collections.reverse(stack);
+        }
+
+        // 3. read instructions
+        /*  read next and throw away
+            read nextInt -> number to move
+            read next and throw away
+            read nextInt -> move from
+            read nextInt -> move to
+        */
+        while(in.hasNext()){
+            in.next();
+            int numToMove = in.nextInt();
+            in.next();
+            int moveFrom = in.nextInt()-1;
+            in.next();
+            int moveTo = in.nextInt()-1;
+
+            Stack<String> boxes = new Stack<String>();
+
+            for(int i = 0; i < numToMove; i++){
+                String box = stacks.get(moveFrom).pop();
+                boxes.push(box);
+            }
+
+            while(!boxes.isEmpty()){
+                stacks.get(moveTo).push(boxes.pop());
+            }
+
+        }
+        String result = "";
+
+        for(Stack<String> stack : stacks){
+            result += stack.pop();
+        }        
+        System.out.println(result);
 
     }
 
     public static void main(String[] args) {
         try {
-            part1();
+            part2();
         } catch (Exception e){
             System.out.println(e);
         }
